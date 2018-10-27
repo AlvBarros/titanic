e464ab21291368372a0fe90e7d4d266106e85043
AlvBarros <alv.barrosc@hotmail.com>
2018-10-27 14:09:24 -0300

    Chances de sobrevivência por sexo e classe

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def to_percentage(v):
+    return v * 100
+
+font = {
+    'family': 'serif'
+}
+
 train = pd.read_csv('../input/train.csv', header=0)
 
-# Chance de sobrevivência por classe
-# train.groupby(['Pclass'], as_index=True)[['Survived']].mean().plot(kind='bar')
+# Chance de sobreviver por sexo
+train.groupby(['Sex'], as_index=True)[['Survived']].mean().applymap(to_percentage).plot(kind='bar')
+plt.figure(2)
+plt.title('Chance de sobrevivência por sexo', fontdict=font)
+plt.xlabel('sexo', fontdict=font)
+plt.ylabel('chance (%)', fontdict=font)
 
+# Chance de sobrevivência por classe
+train.groupby(['Pclass'], as_index=True)[['Survived']].mean().applymap(to_percentage).plot(kind='bar')
+plt.figure(1)
+plt.title('Chance de sobrevivência por classe', fontdict=font)
+plt.xlabel('classe', fontdict=font)
+plt.ylabel('chance (%)', fontdict=font)
+
+plt.show()
